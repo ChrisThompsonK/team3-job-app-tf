9c08610352ac0a482663357a185e6ce7a430bece
Andrew Lyttle <andrew.lyttle@kainos.com>
2025-11-18 15:48:18 +0000

    renamed rbac_authorisation_enabled to enable_rbac_authorisation

--- terraform/main.tf
+++ terraform/main.tf
@@ -7,12 +7,12 @@ resource "azurerm_resource_group" "main" {
 # KEY VAULT - For storing application secrets
 # ============================================
 resource "azurerm_key_vault" "main" {
-  name                       = "kv-team3-jobapp-${var.environment}"
-  location                   = azurerm_resource_group.main.location
-  resource_group_name        = azurerm_resource_group.main.name
-  tenant_id                  = data.azurerm_client_config.current.tenant_id
-  sku_name                   = "standard"
-  purge_protection_enabled   = false
-  rbac_authorization_enabled = true
+  name                        = "kv-team3-jobapp-${var.environment}"
+  location                    = azurerm_resource_group.main.location
+  resource_group_name         = azurerm_resource_group.main.name
+  tenant_id                   = data.azurerm_client_config.current.tenant_id
+  sku_name                    = "standard"
+  purge_protection_enabled    = false
+  enable_rbac_authorization   = true
 }
 
